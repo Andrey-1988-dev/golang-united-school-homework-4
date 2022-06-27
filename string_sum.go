3b28f6365327629f6abf1e8acade37f6ca6e5668
Andrey <andrey.y.n.1988@gmail.com>
2022-06-27 21:35:57 +0300

    #1

--- string_sum.go
+++ string_sum.go
@@ -28,22 +28,62 @@ var (
 func StringSum(input string) (output string, err error) {
 
 	input = strings.TrimSpace(input)
-	if len([]rune(input)) == 0 {
-		return "", fmt.Errorf("error: %v", errorEmptyInput)
+	if len(input) == 0 {
+		//Use when the input is empty, and input is considered empty if the string contains only whitespace
+		return "", fmt.Errorf("inpute errore: %w", errorEmptyInput)
 	}
 
-	strOperands := strings.Split(input, "+")
-	if len(strOperands) != 2 {
-		return "", fmt.Errorf("error: %v", errorNotTwoOperands)
+	// Колличество слогаемых
+	const amountNumber = 2
+
+	// Массив слагаемых в виде string
+	var numbers [amountNumber]string
+
+	// Порядковый номер слагаемого
+	addendum := 0
+
+	// Перебираем руны и  заполняем numbers
+	for _, itemRune := range input {
+		itemString := string(itemRune)
+		if itemString == "+" || itemString == "-" {
+			if len(numbers[addendum]) == 0 {
+				numbers[addendum] += itemString
+			} else {
+				addendum++
+				if addendum == amountNumber {
+					// Use when the expression has number of operands not equal to two
+					return "", fmt.Errorf("new number errore: %w", errorNotTwoOperands)
+				}
+				numbers[addendum] += itemString
+			}
+		} else if itemString == " " {
+			continue
+		} else {
+			_, err := strconv.Atoi(itemString)
+			if err != nil {
+				// For the cases, when the input expression is not valid(contains characters, that are not numbers, +, - or whitespace) the function should return an empty string and an appropriate error from strconv package wrapped into your own error with fmt.Errorf function
+				return "", fmt.Errorf("contains characters, that are not numbers, +, - or whitespace: %w", err)
+			} else {
+				numbers[addendum] += itemString
+			}
+		}
 	}
 
-	var operands [2]int
-	for i, operand := range strOperands {
-		operand = strings.TrimSpace(operand)
-		operands[i], _ = strconv.Atoi(operand)
+	if addendum < amountNumber-1 {
+		// Use when the expression has number of operands not equal to two
+		return "", fmt.Errorf("minimum numbers errore: %w", errorNotTwoOperands)
 	}
 
-	operandsSum := operands[0] + operands[1]
-	output = strconv.Itoa(operandsSum)
+	// сумма всех слогаемых
+	var sum int
+	for _, number := range numbers {
+		intNumber, err := strconv.Atoi(number)
+		if err != nil {
+			return "", fmt.Errorf("error converting string to number: %w", err)
+		}
+		sum += intNumber
+	}
+
+	output = strconv.Itoa(sum)
 	return output, nil
 }
